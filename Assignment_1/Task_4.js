56f94127f17238d684865702b4bde4d36f219211
iSHIVAMPANDEY <i.shivampandeyy@gmail.com>
2026-09-18 13:59:13 +0530

    .

---
+++ Assignment_1/Task_4.js
@@ -0,0 +1,26 @@
+// Task 4 - Student Activity Tracker
+
+const EventEmitter = require('events');
+
+const tracker = new EventEmitter();
+
+tracker.on('login', (name) => {
+    console.log(`${name} logged in`);
+});
+
+tracker.on('courseRegistration', (course) => {
+    console.log(`Course registered: ${course}`);
+});
+
+tracker.on('notification', (msg) => {
+    console.log(`Notification: ${msg}`);
+});
+
+tracker.on('logout', (name) => {
+    console.log(`${name} logged out`);
+});
+
+tracker.emit('login', 'Rahul');
+tracker.emit('courseRegistration', 'Full Stack Development');
+tracker.emit('notification', 'Your course registration is successful');
+tracker.emit('logout', 'Rahul');
