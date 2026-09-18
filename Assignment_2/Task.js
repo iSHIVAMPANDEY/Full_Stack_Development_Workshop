0a89a4440c2caeb39a7e19476c35b66b0273b64e
iSHIVAMPANDEY <i.shivampandeyy@gmail.com>
2026-09-18 14:00:19 +0530

    Assignment 2 pushed

---
+++ Assignment_2/Task.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+
+fs.writeFileSync(
+    'student.txt',
+    'Name: Rahul\nRoll Number: 101\nBranch: CSE\nSemester: 3'
+);
+
+console.log('File created successfully');
+
+let data = fs.readFileSync('student.txt', 'utf8');
+
+console.log(data);
+
+fs.appendFileSync(
+    'student.txt',
+    '\nSubject: Full Stack Development\nMarks: 85\nAttendance: 92%'
+);
+
+console.log('File updated');
+
+data = fs.readFileSync('student.txt', 'utf8');
+
+console.log(data);
